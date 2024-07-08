a73ad2024828c34f051a2a8bb003857b3b5adf6e
Aswathymenonachu <aswathymenonachu92@gmail.com>
2024-07-08 14:43:54 +0400

    initial commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.querySelector(".toggle-theme").addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+});
+document.querySelector(".toggle-theme-mobile").addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+});
